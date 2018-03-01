3d8701ad2a692cd915156fcb78c2f5fc1ecb0b20
Wray Bowling <fabmud@gmail.com>
2018-03-01 17:43:15 -0500

    Test sketch added, but it doesn't work

---
+++ kumquat/kumquat.ino
@@ -0,0 +1,44 @@
+/**
+* Board Type: Mini Gorille CV Graphic / Arduino UNO
+* Description: Alternate Firmware
+*/
+
+// Pins 
+#define mosiPin   17
+#define sclk      19
+#define dc        14
+#define resetPin  15
+#define cs        16
+#define 
+
+// Colors
+#define	BLACK           0x0000
+#define	BLUE            0x001F
+#define	RED             0xF800
+#define	GREEN           0x07E0
+#define CYAN            0x07FF
+#define MAGENTA         0xF81F
+#define YELLOW          0xFFE0  
+#define WHITE           0xFFFF
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1351.h>
+#include <SPI.h>
+
+// Option 1: use any pins but a little slower
+Adafruit_SSD1351 tft = Adafruit_SSD1351(cs, dc, mosiPin, sclk, resetPin);  
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("setup complete");
+  tft.begin();
+}
+
+void loop() {
+  tft.fillScreen(RED);
+  delay(500);
+  tft.fillScreen(GREEN);
+  delay(500);
+  tft.fillScreen(BLUE);
+  delay(500);
+}
