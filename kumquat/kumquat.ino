7f00bda25fbebecda3d964e1df07294a0536d5b2
Wray Bowling <fabmud@gmail.com>
2018-03-01 18:47:05 -0500

    I got a blinking LED!

--- kumquat/kumquat.ino
+++ kumquat/kumquat.ino
@@ -4,11 +4,17 @@
 */
 
 // Pins 
-#define mosiPin       17
-#define sclk          19
-#define dc            14
-#define resetPin      15
-#define cs            16
+#define screen_MOSI_pin                 17
+#define screen_MISO_pin                 '?'
+#define sck 19
+#define screen_externalCLock_pin        19 // ???
+#define screen_internalCLockSelect_pin  19 // ???
+#define screen_DataCommand_pin          14
+#define screen_Reset_pin                15
+#define screen_OLEDChipSelect_pin       16
+#define screen_ReadWrite_pin            '?'
+
+
 #define mgLEDPin      6
 #define ardcoreLEDPin 3 
 
@@ -17,23 +23,24 @@
 #define	BLUE            0x001F
 #define	RED             0xF800
 #define	GREEN           0x07E0
-#define CYAN            0x07FF
-#define MAGENTA         0xF81F
-#define YELLOW          0xFFE0  
-#define WHITE           0xFFFF
 
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1351.h>
 #include <SPI.h>
 
 // Option 1: use any pins but a little slower
-Adafruit_SSD1351 tft = Adafruit_SSD1351(cs, dc, mosiPin, sclk, resetPin);  
+Adafruit_SSD1351 tft = Adafruit_SSD1351(
+  screen_OLEDChipSelect_pin,
+  screen_DataCommand_pin,
+  screen_MOSI_pin,
+  sck,
+  screen_Reset_pin);  
 
 void setup() {
   Serial.begin(9600);
   Serial.println("setup complete");
   tft.begin();
-    pinMode(ardcoreLEDPin, OUTPUT);  
+  pinMode(ardcoreLEDPin, OUTPUT);  
   pinMode(mgLEDPin, OUTPUT);
 }
 
@@ -41,11 +48,8 @@ void loop() {
   tft.fillScreen(RED);
   digitalWrite(mgLEDPin, HIGH);
   digitalWrite(ardcoreLEDPin, HIGH);
-  delay(500);
   tft.fillScreen(GREEN);
   digitalWrite(mgLEDPin, LOW);
   digitalWrite(ardcoreLEDPin, LOW);
-  delay(500);
-  tft.fillScreen(BLUE);
-  delay(500);
+//  tft.fillScreen(BLUE);
 }
